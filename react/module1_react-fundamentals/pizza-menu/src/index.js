dd347c305feaf52b72084197ed35030039d6a65b
FaisalKhan2000 <iamfaisal.luv@gmail.com>
2024-01-03 11:20:44 +0530

    completed pizza project

--- react/module1_react-fundamentals/pizza-menu/src/index.js
+++ react/module1_react-fundamentals/pizza-menu/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 // import { ReactDOM } from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 
 const pizzaData = [
@@ -66,11 +67,69 @@ function Header() {
   );
 }
 
+{
+  /* 
+        Here we are using map function to pass a prop to the child which is pizza
+        pizzaData.map((pizza) => pizza) behvaes differently than javascript
+        
+        Array.map() in React behaves similarly to its behavior in JavaScript, but its usage within React has a specific context and impact due to React's rendering and reconciliation process.
+
+        In JavaScript, Array.map() is a method used to iterate over an array and execute a provided function on each element of that array, returning a new array based on the results of the function. This behavior remains the same in React.
+
+        However, the difference lies in how React handles the elements generated from Array.map() when they're components that will be rendered in the JSX. When you use Array.map() in React to generate components (like <Pizza /> components), React tracks each component instance created within the map() loop separately.
+        
+
+        const NumberList = () => {
+        const numbers = [1, 2, 3, 4, 5];
+
+        return (
+          <div>
+            <h2>Number List</h2>
+            <ul>
+              {numbers.map((number) => (
+                <li key={number}>{number}</li>
+              ))}
+            </ul>
+          </div>
+        );
+      };
+      Output: 
+        Number List
+        - 1
+        - 2
+        - 3
+        - 4
+        - 5
+
+
+        */
+}
+
+// Menu() is the parent of Pizza()
 function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza
+      {numPizzas > 0 ? (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We're still working on our menu. Please come back later</p>
+      )}
+      {/* <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
         photoName="./pizzas/spinaci.jpg"
@@ -87,46 +146,63 @@ function Menu() {
         ingredients="Bread with italian olive oil and rosemary"
         photoName="./pizzas/focaccia.jpg"
         price={13}
-      />
+      /> */}
     </main>
   );
 }
 
-function Pizza(props) {
-  console.log(props);
+function Pizza({ pizzaObj }) {
+  // console.log(pizzaObj);
+
+  // if (pizzaObj.soldOut) return null;
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()} We're currently open
+      {isOpen ? (
+        <Order closeHour={closeHour} openHour={openHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
     </footer>
   );
 }
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
+function Order({ closeHour, openHour }) {
+  return (
+    <div className="order">
+      <p>
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
+}
 
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-// ReactDOM.render(<App />, document.getElementById("root"));
-
-ReactDOM.render(
-  <React.StrictMode>
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
