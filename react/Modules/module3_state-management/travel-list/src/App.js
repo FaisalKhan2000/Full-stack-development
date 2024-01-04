cf3bf73b1839bcf4588385018f03cb4e99bc4e54
FaisalKhan2000 <iamfaisal.luv@gmail.com>
2024-01-04 12:54:41 +0530

    adding files -- state in react

---
+++ react/Modules/module3_state-management/travel-list/src/App.js
@@ -0,0 +1,107 @@
+import { useState } from "react";
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "specs", quantity: 2, packed: false },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+
+function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // adding condition to not return anything if description is empty
+    if (!description) {
+      return;
+    }
+
+    const newItem = { description, quantity, package: false, id: Date.now() };
+    console.log(newItem);
+
+    //  Resetting the form to initial state
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip</h3>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          setQuantity(Number(e.target.value));
+          console.log(Number(e.target.value));
+        }}
+      >
+        {/* 
+      The code generates sequential numbers from 1 to 20 and then gets used to generate 20 options.
+
+      Array.from() is a built-in JS method that creates a new array from an iterable or an array-like object (objects with a length property and indexed elements). 
+      The first argument of { length: 20 }, specifies the desired length of the array. The second argument is an optional mapping function that determines the values of the array elements. In this case, the mapping function takes two parameters: _ for the element value (undefined), and i for the zero-based index. The mapping function returns i + 1 as the value for each element. The resulting array will have a length of 20 and contain the values  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] . */}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => {
+          console.log(e.target.value);
+          setDescription(e.target.value);
+        }}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
