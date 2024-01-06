13da6b2719fe6d34f2f54c7f746a4abc57d3c30d
FaisalKhan2000 <iamfaisal.luv@gmail.com>
2024-01-06 12:28:22 +0530

    adding files

--- react/Modules/module3_state-management/travel-list/src/App.js
+++ react/Modules/module3_state-management/travel-list/src/App.js
@@ -3,6 +3,7 @@ const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
   { id: 3, description: "specs", quantity: 2, packed: false },
+  { id: 4, description: "pants", quantity: 2, packed: false },
 ];
 
 export default function App() {
