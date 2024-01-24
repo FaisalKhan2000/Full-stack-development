19b495fe92de85829857362c891be583594ff705
FaisalKhan2000 <iamfaisal.luv@gmail.com>
2024-01-24 13:59:53 +0530

    removing API

--- react/react-projects/weather-app/src/api.js
+++ react/react-projects/weather-app/src/api.js
@@ -10,16 +10,3 @@
 // export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/";
 
 // export const WEATHER_API_KEY = "YOUR_WEATHER_API";
-
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
-export const geoApiOptions = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "626db8b6d9msh50a1e1a7cc555d6p14ec4bjsn487714e29fd3",
-    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
-  },
-};
-
-export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/";
-
-export const WEATHER_API_KEY = "a8a2d720fa166493dd402f9fe3ed9fc5";
