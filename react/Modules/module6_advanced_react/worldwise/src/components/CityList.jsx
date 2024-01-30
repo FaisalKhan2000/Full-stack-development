0f07f052945960dd7570cdd2909db507010d39e8
FaisalKhan2000 <iamfaisal.luv@gmail.com>
2024-01-30 12:17:51 +0530

    chnaoao

--- react/Modules/module6_advanced_react/worldwise/src/components/CityList.jsx
+++ react/Modules/module6_advanced_react/worldwise/src/components/CityList.jsx
@@ -2,8 +2,10 @@ import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
+import { useCities } from "../Contexts/CitiesContext";
 
-export default function CityList({ cities, isLoading }) {
+export default function CityList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
   if (!cities.length)
     return (
--- react/Modules/module6_advanced_react/worldwise/src/components/CountryList.jsx
+++ react/Modules/module6_advanced_react/worldwise/src/components/CountryList.jsx
@@ -2,8 +2,10 @@ import CountryItem from "./CountryItem";
 import styles from "./CountryList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
+import { useCities } from "../Contexts/CitiesContext";
 
-export default function CountryList({ cities, isLoading }) {
+export default function CountryList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
   if (!cities.length)
     return (
