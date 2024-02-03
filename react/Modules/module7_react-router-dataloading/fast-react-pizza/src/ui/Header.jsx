57d3949393f5a92469b82974f4828403e6af3100
FaisalKhan2000 <iamfaisal.luv@gmail.com>
2024-02-03 12:23:47 +0530

    adding fast react pizz co. project

---
+++ react/Modules/module7_react-router-dataloading/fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+
+      <SearchOrder />
+      <p>faisal</p>
+    </header>
+  );
+}
+
+export default Header;
