a13fa6b1c013fd082c73f6caac4761bcb9b4d017
FaisalKhan2000 <iamfaisal.luv@gmail.com>
2024-02-04 17:12:29 +0530

    adding projects

---
+++ react/react-concepts/4_useReducer/02reactQuiz/src/components/FinishScreen.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function FinishScreen({
+  points,
+  maxPossiblePoints,
+  highscore,
+  dispatch,
+}) {
+  const percentage = (points / maxPossiblePoints) * 100;
+  return (
+    <>
+      <p className="result">
+        You scored <strong>{points}</strong> out of {maxPossiblePoints} (
+        {Math.ceil(percentage)}%)
+      </p>
+
+      <p className="highscore">(Highscore: {highscore} points)</p>
+
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart Quiz
+      </button>
+    </>
+  );
+}
