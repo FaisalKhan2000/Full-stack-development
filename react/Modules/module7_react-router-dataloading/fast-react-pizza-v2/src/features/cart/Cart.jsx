9e0fe4a475e757992bebda46d367b13e725dad62
FaisalKhan2000 <iamfaisal.luv@gmail.com>
2024-02-06 14:42:14 +0530

    adding additional files for Fast React Pizza Co.

--- react/Modules/module7_react-router-dataloading/fast-react-pizza-v2/src/features/cart/Cart.jsx
+++ react/Modules/module7_react-router-dataloading/fast-react-pizza-v2/src/features/cart/Cart.jsx
@@ -1,43 +1,51 @@
 import CartItem from "./CartItem";
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart, getCart } from "./cartSlice";
+import { getUser } from "../users/userSlice";
+import EmptyCart from "../cart/EmptyCart";
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+// const fakeCart = [
+//   {
+//     pizzaId: 12,
+//     name: "Mediterranean",
+//     quantity: 2,
+//     unitPrice: 16,
+//     totalPrice: 32,
+//   },
+//   {
+//     pizzaId: 6,
+//     name: "Vegetale",
+//     quantity: 1,
+//     unitPrice: 13,
+//     totalPrice: 13,
+//   },
+//   {
+//     pizzaId: 11,
+//     name: "Spinach and Mushroom",
+//     quantity: 1,
+//     unitPrice: 15,
+//     totalPrice: 15,
+//   },
+// ];
 
 function Cart() {
-  const cart = fakeCart;
+  const dispatch = useDispatch();
+  const username = useSelector(getUser);
+  const cart = useSelector(getCart);
+
+  if (!cart.length) return <EmptyCart />;
 
   return (
     <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
 
       <ul className="mt-3 divide-y divide-stone-200 border-b ">
         {cart.map((item) => (
-          <CartItem item={item} key={item.key} />
+          <CartItem item={item} key={item.pizzaId} />
         ))}
       </ul>
 
@@ -45,7 +53,9 @@ function Cart() {
         <Button type="primary" to="/order/new">
           Order pizzas
         </Button>
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={() => dispatch(clearCart())}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
--- react/Modules/module7_react-router-dataloading/fast-react-pizza-v2/src/features/users/Username.jsx
+++ react/Modules/module7_react-router-dataloading/fast-react-pizza-v2/src/features/users/Username.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getUser } from "./userSlice";
 
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Faisal</div>;
+  const username = useSelector(getUser);
+  // console.log(username);
+
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
 export default Username;
