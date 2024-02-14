84f8f720cdbd1bba1ab40ce07c5604a35caf87de
FaisalKhan2000 <iamfaisal.luv@gmail.com>
2024-02-14 20:36:49 +0530

    adding

--- react/react-concepts/9_redux/02userDataFetchingRTK/src/components/Users.jsx
+++ react/react-concepts/9_redux/02userDataFetchingRTK/src/components/Users.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from "../store/usersSlice";
+import { STATUSES, fetchUsers } from "../store/usersSlice";
 
 function Users() {
   const { data: users, status } = useSelector((state) => state.users);
   console.log(users);
+  console.log(status);
 
   const dispatch = useDispatch();
   useEffect(function () {
     dispatch(fetchUsers());
   }, []);
+  if (status === STATUSES.LOADING) return <div>Loading....</div>;
+
+  if (status === STATUSES.ERROR) return <div>Error loading data</div>;
   return <div></div>;
 }
 
