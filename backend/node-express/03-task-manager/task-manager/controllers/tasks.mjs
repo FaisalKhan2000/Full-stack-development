dedb5b26032d198d146650267c01f54ba4d75cb4
FaisalKhan2000 <iamfaisal.luv@gmail.com>
2024-02-29 16:29:25 +0530

    some changes

--- backend/node-express/03-task-manager/task-manager/controllers/tasks.mjs
+++ backend/node-express/03-task-manager/task-manager/controllers/tasks.mjs
@@ -53,21 +53,6 @@ const updateTask = async (req, res, next) => {
   res.status(200).json({ task });
 };
 
-// const updateTask = asyncWrapper(async (req, res, next) => {
-//   const { id: taskID } = req.params;
-
-// const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-//   new: true,
-//   runValidators: true,
-// });
-
-//   if (!task) {
-//     return next(createCustomError(`No task with id : ${taskID}`, 404));
-//   }
-
-//   res.status(200).json({ task });
-// });
-
 export { getAllTasks, createTask, getSingleTask, deleteTask, updateTask };
 
 // const getAllTasks = asyncWrapper(async (req, res) => {
